2f3a7f3d1c5a6d9948bac243513b9c17d6c66ab7
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-03 01:34:08 +0530

    ADDED Code to validate given first name.

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,1 +1,11 @@
-console.log("Welcome to User Registration.");
+console.log("Welcome to User Registration.\n");
+
+const prompt = require('prompt-sync')();
+
+let namePattern = new RegExp("^[A-Z][A-Za-z0-9]{2,}$");
+
+let name = prompt("Enter your First name:  ");
+if(namePattern.test(name))
+    console.log(name + " Your first name is saved");
+else
+    console.log("Invalid Name");
