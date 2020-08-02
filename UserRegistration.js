017a81ae7dee21d451b24e964b3f6cf838f59c5d
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-03 02:34:42 +0530

    ADDED Code to validate last name.

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,11 +1,38 @@
 console.log("Welcome to User Registration.\n");
 
+console.log("Name should start with Capital letter and should have minimum 3 characters.\n")
+
 const prompt = require('prompt-sync')();
+const namePattern = new RegExp("^[A-Z][A-Za-z0-9]{2,}$");
 
-let namePattern = new RegExp("^[A-Z][A-Za-z0-9]{2,}$");
+function checkValidation(name, namePattern)
+{
+    let flag
+    if(namePattern.test(name))
+    {
+        console.log("Saved\n");
+        flag = false;
+    }    
+    else
+    {
+        console.log("Invalid Input enter again.\n");
+        flag = true;
+    }   
+    return flag; 
+}
 
-let name = prompt("Enter your First name:  ");
-if(namePattern.test(name))
-    console.log(name + " Your first name is saved");
-else
-    console.log("Invalid Name");
+while(true)
+{
+    let firstName = prompt("Enter your First name:  ");
+    let check = checkValidation(firstName, namePattern);
+    if(check == false)
+        break;
+}
+
+while(true)
+{
+    let lastName = prompt("Enter your Last name:  ");
+    let check = checkValidation(lastName, namePattern)
+    if(check == false)
+        break;
+}
